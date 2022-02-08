55d11c6aa1ec1a178bf990ab9bc70dfb2780a008
Ramon <ramon_max1986@hotmail.com>
2022-02-08 17:04:11 -0300

    Finished

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function addItem(){
+function addItem() {
 
     let list = document.querySelector("ul.mainList")
     let newListItem = document.createElement("li")
- 
+
     //Descobrir quant items de uma list
     //Se impar adicionar red ou par adicionar white 
     console.log(list.childElementCount);
@@ -11,7 +11,7 @@ function addItem(){
     let listQuantity = list.childElementCount;
     console.log(listQuantity)
 
-    if(listQuantity%2 == 1){
+    if (listQuantity % 2 == 1) {
         newListItem.setAttribute("class", "listItemWhite")
     } else {
         newListItem.setAttribute("class", "listItem")
@@ -19,32 +19,33 @@ function addItem(){
 
     /*Item Text*/
     let itemTyped = document.querySelector("input.inputItem").value
-    let itemResult = document.createElement("p")
-    let newText = document.createTextNode(itemTyped)
-    itemResult.appendChild(newText)
-    //riscando o item que já foi feito
-    itemResult.addEventListener("click", function checked(event){
-        event.target.style.textDecoration = "line-through";
-    })
 
-    let deleteButton = document.createElement("p")
-    let deleteButtonText = document.createTextNode("X")
-    deleteButton.setAttribute("class", "deleteButton")
-    deleteButton.appendChild(deleteButtonText)
+    if (itemTyped == '') {
+        window.alert("We do not accept empty items")
+    } else {
+        let itemResult = document.createElement("p")
+        itemResult.setAttribute("class", "paragraphText")
+        let newText = document.createTextNode(itemTyped)
+        itemResult.appendChild(newText)
+        //riscando o item que já foi feito
+        itemResult.addEventListener("click", function checked(event) {
+            event.target.style.textDecoration = "line-through";
+        })
 
-    deleteButton.addEventListener("click", function deleteItem(deleteItem){
-        deleteItem.target.parentElement.style.display = "none"
-    })
+        let deleteButton = document.createElement("p")
+        let deleteButtonText = document.createTextNode("X")
+        deleteButton.setAttribute("class", "deleteButton")
+        deleteButton.appendChild(deleteButtonText)
 
-    newListItem.appendChild(itemResult)
-    newListItem.appendChild(deleteButton)
-    list.appendChild(newListItem)
- 
+        deleteButton.addEventListener("click", function deleteItem(deleteItem) {
+            let deletedItem = deleteItem.target.parentElement
+            deletedItem.remove()
+        //o de baixo tbm funciona mas o elemento continua existindo
+/*         deleteItem.target.parentElement.style.display = "none"
+ */    })
+
+        newListItem.appendChild(itemResult)
+        newListItem.appendChild(deleteButton)
+        list.appendChild(newListItem)
+    }
 }
-
-// Do not let user type empty values
-// change color of element when deletes
-// Add press Enter and add item
-
-
-
